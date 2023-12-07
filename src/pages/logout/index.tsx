db24d21b1411a67d2fa3ffe1f716a977b74892db
Adam Szokalski <szokalskiadam@gmail.com>
2023-12-07 12:58:57 +0100

    fixed logout

--- src/pages/logout/index.tsx
+++ src/pages/logout/index.tsx
@@ -1,12 +1,14 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import jscookie from "js-cookie";
 
 export default function Logout() {
-    const router = useRouter(); 
-    useEffect(()=>{
-        sessionStorage.removeItem("token");
-        sessionStorage.removeItem("username");
+    const router = useRouter();
+    useEffect(() => {
+        jscookie.remove("token");
+        jscookie.remove("username");
+
         router.push("/login");
-    }, [router])
+    }, [router]);
     return <></>;
 }
