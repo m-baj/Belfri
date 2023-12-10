578d905e1f0a7bbd8e64ffd3fbd01f2fe533401a
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-12-10 16:42:10 +0100

    Added header background

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -7,10 +7,11 @@ import { useRouter } from 'next/router';
 
 export default function Header() {
     return (
-        <Flex>
-            <Typography.Title style={{ color: blue[4], textAlign: "left" }}>
-                {config.appName}
-            </Typography.Title>
+        <Flex justify='space-between' align='center'
+              style={{ background: blue[1], width: '100%', height: '100px' }}>
+                <Typography.Title style={{ color: blue[4], textAlign: "left", padding: 16 }}>
+                    {config.appName}
+                </Typography.Title>
         </Flex>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,14 @@ import Link from "next/link";
 export default function Home() {
     const { token, username } = useUser();
     return (
-        <Flex vertical>
-            <PageHeader />
-            <Flex>
-            {username} : {token}<br />
-            <Link href="/logout">Logout</Link>
+        <body style={{ margin: 0 }}>
+            <Flex vertical>
+                <PageHeader />
+                <Flex>
+                    {username} : {token}<br />
+                    <Link href="/logout">Logout</Link>
+                </Flex>
             </Flex>
-        </Flex>
+        </body>
     );
 }
