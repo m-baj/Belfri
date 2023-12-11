a8fd1c9402b3b2696822d9167de9bd542923097b
Adam Szokalski <szokalskiadam@gmail.com>
2023-12-11 11:47:16 +0100

    Token generation

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,9 +4,9 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { blue } from "@ant-design/colors";
 import config from "@/configs/app.config";
 import axios from "axios";
-import { sha256 } from "js-sha256";
+import {sha256,} from 'js-sha256'
 import { useRouter } from "next/router";
-import jscookie from "js-cookie";
+import jscookie from 'js-cookie'
 
 interface Fields {
     username?: string;
@@ -17,41 +17,36 @@ interface Fields {
 export default function LoginForm() {
     const [form] = Form.useForm();
     const router = useRouter();
-
+    
     const handleForm = (values: any) => {
         console.log(sha256(values.password));
-        axios
-            .post("/api/login/", {
-                username: values.username,
-                passHash: sha256(values.password),
-            })
-            .then((res) => {
-                if (res.status == 200) {
-                    const token = res.data.token;
-                    jscookie.set("token", token, { expires: 1 });
-                    jscookie.set("username", values.username), { expires: 1 };
-                    router.push("/");
-                } else {
-                    message.error("Login failed");
-                }
-            })
-            .catch((err) => {
-                if (err.response.status == 401) {
-                    form.setFields([
-                        {
-                            name: "password",
-                            errors: [err.response.data.message],
-                        },
-                    ]);
-                } else {
-                    message.error(
-                        "Login failed due to server error: " +
-                            err.response.status +
-                            " " +
-                            err.response.data.message
-                    );
-                }
-            });
+        axios.post("/api/login/", {
+            username: values.username,
+            passHash: sha256(values.password),
+        }).then((res) => {
+            if(res.status == 200){
+                const token = res.data.token;            
+                jscookie.set('token', token, {expires: 1});
+                jscookie.set('username', values.username), {expires: 1};
+                router.push("/");
+            } 
+            else {
+                message.error("Login failed");
+            }
+        }
+        ).catch((err) => {
+            if (err.response.status == 401) {
+                form.setFields([
+                    {
+                        name: "password",
+                        errors: [err.response.data.message],
+                    },
+                ]);
+            } else {
+                message.error("Login failed due to server error: " + err.response.status + " " + err.response.data.message);
+            }
+     
+        });
     };
 
     return (
@@ -63,7 +58,7 @@ export default function LoginForm() {
             <Form
                 data-testid="login-form"
                 form={form}
-                name="loginform"
+                name="loginorm"
                 className={style.LoginForm}
                 initialValues={{ remember: true }}
                 autoCapitalize="off"
@@ -81,7 +76,7 @@ export default function LoginForm() {
                     <Input
                         size="large"
                         placeholder="Username"
-                        prefix={<UserOutlined style={{ color: blue[4] }} />}
+                        prefix={<UserOutlined color="blue" />}
                     />
                 </Form.Item>
                 <Form.Item<Fields>
@@ -96,7 +91,7 @@ export default function LoginForm() {
                     <Input.Password
                         size="large"
                         placeholder="Password"
-                        prefix={<LockOutlined style={{ color: blue[4] }} />}
+                        prefix={<LockOutlined color="blue" />}
                     />
                 </Form.Item>
                 <Flex justify="space-evenly">
@@ -104,9 +99,7 @@ export default function LoginForm() {
                         <Checkbox>Remember me</Checkbox>
                     </Form.Item>
                     <Form.Item<Fields>>
-                        <Button className={style.rightAlignButton} type="link">
-                            Forgot password?
-                        </Button>
+                        <Button className={style.rightAlignButton}type="link">Forgot password?</Button>
                     </Form.Item>
                 </Flex>
                 <Form.Item<Fields>>
--- src/utils/database/Connection.ts
+++ src/utils/database/Connection.ts
@@ -3,27 +3,27 @@ import { getUserAuth } from "./queries/authorize";
 
 // Define the database connection details
 const dbConfig: OracleDB.ConnectionAttributes = {
-    user: 'aszokals',
-    password: 'aszokals',
-    connectString: 'ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl',
-  };
+    user: "aszokals",
+    password: "aszokals",
+    connectString: "ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl",
+};
 
 enum AuthLevel {
     ADMIN = 3,
-    TUTOR = 2,   
+    TUTOR = 2,
     STUDENT = 1,
-    GUEST = 0
+    GUEST = 0,
 }
 
 /* The Connection class is a TypeScript class that represents a connection to an Oracle database and
 provides methods for executing SQL queries and managing authorization levels. */
 
-export default class Connection{
+export default class Connection {
     private connection: OracleDB.Connection;
     private authLevel: AuthLevel = AuthLevel.GUEST;
     private username: string | null = null;
-    
-    constructor (connection: OracleDB.Connection) {
+
+    constructor(connection: OracleDB.Connection) {
         this.connection = connection;
     }
 
@@ -36,11 +36,18 @@ export default class Connection{
         await this.connection.close();
     }
 
-    public async execute(sql: string, bindParams?: OracleDB.BindParameters ): Promise<OracleDB.Result<any>> {
+    public async execute(
+        sql: string,
+        bindParams?: OracleDB.BindParameters
+    ): Promise<OracleDB.Result<any>> {
         if (bindParams === undefined) {
             return await this.connection.execute(sql);
         }
         return await this.connection.execute(sql, bindParams);
+    }
+
+    public async commit(): Promise<void> {
+        await this.connection.commit();
     }
 
     public async authorize(token: string): Promise<boolean> {
