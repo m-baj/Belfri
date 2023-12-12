7d54adf30374cdc6dae3eeb9ee58d5a2922bef5b
Adam Kras <adampkras@gmail.com>
2023-12-13 00:40:47 +0100

    Update of the previous commit

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,29 +1,13 @@
-// import { useUser } from "@/hooks/useUser";
-// import Login from "./Login";
-// import LoadingComponent from "@/components/LoadingComponent/LoadingComponent";
-
-// export default function Loader() {
-//     const { loading } = useUser({
-//         loggedIn: false,
-//         redirectTo: "/",
-//     });
-
-//     if (loading) return <LoadingComponent />;
-//     return <Login />;
-// }
-
 import { useUser } from "@/hooks/useUser";
-import { useRouter } from 'next/router';
+import Login from "./Login";
 import LoadingComponent from "@/components/LoadingComponent/LoadingComponent";
 
 export default function Loader() {
     const { loading } = useUser({
-        loggedIn: true,
+        loggedIn: false,
         redirectTo: "/",
     });
 
-    const router = useRouter();
-
     if (loading) return <LoadingComponent />;
-    router.push('/');
+    return <Login />;
 }
