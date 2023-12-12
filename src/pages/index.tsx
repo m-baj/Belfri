7ec161e98f3924e2046c01ed711adaa93aa83b04
Adam Kras <adampkras@gmail.com>
2023-12-13 00:38:00 +0100

    Removed need for logging in (testing purposes)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,14 @@ const { Header, Footer, Content } = Layout;
 
 
 export default function Home() {
-    const { token, username } = useUser();
+    // const { token, username } = useUser();
     return (
         <body style={{ margin: 0 }}>
             <Header style={{ height: '100px', background: blue[1], padding: 0 }}>
                 <PageHeader />
             </Header>
             <Content>
-                {username} : {token}<br />
+                {/* {username} : {token}<br /> */}
                 <Link href="/logout">Logout</Link>
             </Content>
         </body>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,29 @@
+// import { useUser } from "@/hooks/useUser";
+// import Login from "./Login";
+// import LoadingComponent from "@/components/LoadingComponent/LoadingComponent";
+
+// export default function Loader() {
+//     const { loading } = useUser({
+//         loggedIn: false,
+//         redirectTo: "/",
+//     });
+
+//     if (loading) return <LoadingComponent />;
+//     return <Login />;
+// }
+
 import { useUser } from "@/hooks/useUser";
-import Login from "./Login";
+import { useRouter } from 'next/router';
 import LoadingComponent from "@/components/LoadingComponent/LoadingComponent";
 
 export default function Loader() {
     const { loading } = useUser({
-        loggedIn: false,
+        loggedIn: true,
         redirectTo: "/",
     });
 
+    const router = useRouter();
+
     if (loading) return <LoadingComponent />;
-    return <Login />;
+    router.push('/');
 }
