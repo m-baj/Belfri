1d06af447576080e210e83637133aa4f09f08808
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2023-12-30 00:47:42 +0100

    New buttons in Login Register button has been splitted into register as a student and register as a teacher buttons.

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,12 +1,16 @@
 import { ComponentPreview, Previews } from "@react-buddy/ide-toolbox-next";
 import { PaletteTree } from "./palette";
 import LoginForm from "@/components/LoginForm/LoginForm";
+import Login from "@/pages/login/Login";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree />}>
             <ComponentPreview path="/LoginForm">
                 <LoginForm />
+            </ComponentPreview>
+            <ComponentPreview path="/Login">
+                <Login />
             </ComponentPreview>
         </Previews>
     );
