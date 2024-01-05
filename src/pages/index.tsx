59d0562849127470fb4cf31c38f924f4a6a80c97
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-01-06 00:37:45 +0100

    Removed excessive imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import PageHeader from "@/components/PageHeader/PageHeader";
 // import { useUser } from "@/hooks/useUser";
-import { blue } from "@ant-design/colors";
-import { Layout, Divider } from "antd";
+import { Divider } from "antd";
 import Link from "next/link";
 // import { useEffect, useState } from "react";
-
-
-const { Header, Footer, Content } = Layout;
-
 
 export default function Home() {
     // const { token, username } = useUser();
