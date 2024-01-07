f1cecbb032aa36d7eec6618df8d28e3a52b298eb
Maksymilian <bajmaks@gmail.com>
2024-01-07 17:23:57 +0100

    fixed activation screen

--- src/components/AccountActivationScreen/AccountActivationScreen.tsx
+++ src/components/AccountActivationScreen/AccountActivationScreen.tsx
@@ -1,35 +1,18 @@
-import { Button, Flex, message, Typography } from "antd";
+import { Button, Flex, message, Statistic, Typography } from "antd";
 import { blue } from "@ant-design/colors";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AccountActivationScreen() {
 
-    const [messageApi, contextHolder] = message.useMessage();
-    let lastSentTime = 0;
     const router = useRouter();
     const email = router.query.email;
     const token = router.query.token;
     const name = router.query.name;
 
-    const error = () => {
-        messageApi.open({
-            type: "error",
-            content: "You can send email again in 30 seconds.",
-            duration: 5,
-        });
-    }
-
+    const [formLoading, setFormLoading] = useState(true);
     const sendEmail = () => {
-
-        const currentTimestamp = new Date().getTime();
-        const timeDifference = currentTimestamp - lastSentTime;
-        console.log(timeDifference);
-        if (timeDifference < 30000) {
-            return messageApi.error("You can send email again in 30 seconds.");
-        }
-
         axios
             .post("/api/user/send-activation-email", {
                 "name": name,
@@ -38,33 +21,38 @@ export default function AccountActivationScreen() {
             })
             .then((response) => {
                 if (response.status === 200) {
-                    messageApi.success("Email sent.");
+                    message.success("Email sent");
                 }
             })
             .catch((error) => {
                 message.error("Error: " + error.response.data.message);
             });
+
+        setFormLoading(true);
     }
-    useEffect(() => {
-        sendEmail();
-    })
 
     return (
-        <Flex vertical align="center" style={{ height: "100vh", width: "600"}}>
-            <div>
+        <Flex vertical align="center">
                 <Typography.Title level={2} style={{color: blue[4], textAlign: "center"}}>
                     Account activation
                 </Typography.Title>
                 <Typography.Text style={{ textAlign: "center"}}>
-                    We have sent you an email on address you provided in registration form.
-                    Please check your inbox and click on the link to activate your account.
+                    We have sent you an email on address you provided in registration form. <br/>
+                    Please check your inbox and click on the link to activate your account. <br/> <br/>
                 </Typography.Text>
                 <Flex justify="center">
-                    <Button type="primary" onClick={sendEmail}>
-                        Send email again
+                    <Button type="primary" onClick={sendEmail} disabled={formLoading}>
+                        {formLoading ? <Statistic.Countdown
+                            valueStyle={{ fontSize: "12pt" }}
+                            prefix={"Resend email in "}
+                            format="s"
+                            suffix={"s"}
+                            value={Date.now() + 30000}
+                            onFinish={() => setFormLoading(false)}
+                        /> : "Resend email"}
                     </Button>
                 </Flex>
-            </div>
         </Flex>
+
     );
 }
