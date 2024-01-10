ef4a9c59f1663f9a512e6fef491e48c58d713998
Adam Szokalski <szokalskiadam@gmail.com>
2024-01-10 10:50:26 +0100

    claiming credits

--- src/utils/database/queries/credits/claim/claim.ts
+++ src/utils/database/queries/credits/claim/claim.ts
@@ -5,6 +5,16 @@ export async function claimCredits(connection: Connection, token: string) {
         throw new Error("Unauthorized");
     }
 
+    // check if token exists
+    const result = await connection.execute`SELECT *
+                                            FROM credits_tokens
+                                            WHERE token = ${token}
+                                                FETCH NEXT 1 ROW ONLY`;
+
+    if (!result || !result.rows || result.rows.length == 0) {
+        throw new Error("Invalid token");
+    }
+
     connection.execute`UPDATE users
                        SET credits_id = (SELECT credits_id FROM credits_tokens WHERE token = ${token})
                        WHERE user_id = ${connection.getUserID()}`;
