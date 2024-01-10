d93333a1280a35889159fa18810d1ca500a2523e
Adam Szokalski <szokalskiadam@gmail.com>
2024-01-10 11:54:31 +0100

    fixed description

--- src/components/OfferScreen/OfferDescription/OfferDescription.tsx
+++ src/components/OfferScreen/OfferDescription/OfferDescription.tsx
@@ -7,23 +7,34 @@ interface OfferDescriptionProps {
     offerDescription: string;
 }
 
-export default function OfferDescription( {offerName, offerDescription}: OfferDescriptionProps) {
+export default function OfferDescription({ offerName, offerDescription }: OfferDescriptionProps) {
 
-    const paragraphs = offerDescription.split('\n').map((paragraph, index) => {
-       return ( <Typography.Paragraph key={index} style={{marginLeft: "10%", marginRight: "10%", marginBottom: "0"}}>
+    const paragraphs = offerDescription.split("\n").map((paragraph, index) => {
+        return (<Typography.Paragraph key={index} style={{ marginLeft: "10%", marginRight: "10%", marginBottom: "0" }}>
             {paragraph}
-        </Typography.Paragraph> )
+        </Typography.Paragraph>);
     });
 
     return (
         <Flex vertical align="flex-start" style={
-            {justifyContent: "center"
+            {
+                justifyContent: "center"
             }}>
 
-            <Typography.Title level={ 1 } style={{color: blue[4], width: "100%", marginLeft: "10%"}}>{ offerName }</Typography.Title>
-                <div style={{borderLeft: "1px solid", borderRight: "1px solid", borderLeftStyle: "groove", borderRightStyle: "groove"}}>
-                    {paragraphs}
-                </div>
+            <Typography.Title level={1} style={{
+                color: blue[4],
+                width: "100%",
+                marginLeft: "10%"
+            }}>{offerName}</Typography.Title>
+            <div style={{
+                borderLeft: "1px solid",
+                borderRight: "1px solid",
+                borderLeftStyle: "groove",
+                borderRightStyle: "groove",
+                height: "80vh"
+            }}>
+                {paragraphs}
+            </div>
         </Flex>
     );
 }
