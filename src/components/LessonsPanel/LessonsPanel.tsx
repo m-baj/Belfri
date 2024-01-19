f3ce45a1f8f4579da84c5a1b7dcc25ce4f2608ba
Adam Kraś <adam.kras.stud@pw.edu.pl>
2024-01-20 00:11:36 +0100

    Modified Lessons Panel layout

---
+++ src/components/LessonsPanel/LessonsPanel.tsx
@@ -0,0 +1,12 @@
+import LessonCard from '../LessonCard/LessonCard';
+import { Typography, Flex } from 'antd';
+import { blue } from '@ant-design/colors';
+
+export default function LessonsPanel() {
+    return (
+        <Flex vertical align='center'>
+            <Typography.Title style={{ color: blue[4] }} >Your Lessons</Typography.Title>
+            tu wlecą lekcje
+        </Flex>
+    )
+}
